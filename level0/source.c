b8beb2b80f1d4a266df4d9bb15dd2a7780e8dfbe
Manon BOUQUET <mbouquet@bess-f4r1s13.clusters.42paris.fr>
2024-02-15 16:19:52 +0100

    the executable's C code is added in level0

---
+++ level0/source.c
@@ -0,0 +1,34 @@
+#define _GNU_SOURCE 
+
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+int main (int ac, char *av[])
+{
+    int n = atoi(av[1]);
+    (void)ac;
+
+    if (n == 423)
+    {
+        char *name = strdup("/bin/sh");
+        char *arg[2] = { name, NULL };
+
+        gid_t gid = getegid();
+        uid_t uid = geteuid();
+
+        setresgid(gid, gid, gid);
+        setresuid(uid, uid, uid);
+
+        execv(name, arg);
+    }
+    else 
+    {
+        printf("No !");
+    }
+
+    return 0;
+}
