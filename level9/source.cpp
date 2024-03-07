0a397fed659c6dad1b617f06e0b6230f46321978
tsiguenz <tsiguenz@student.42.fr>
2024-03-07 11:13:48 +0100

    fix source code level9

--- level9/source.cpp
+++ level9/source.cpp
@@ -2,32 +2,25 @@
 #include <cstring>
 
 class N {
-    public:
+public:
+  int operator+(N &rhs) { return this->number + rhs.number; }
 
-    int operator+(N &rhs) {
-      return this->nb + rhs.nb;
-    }
+  char buffer[100];
+  int number;
 
-    char buffer[100];
-    int number;
+  N(int n) : number(n) {}
 
-    N (int n) : number(n) {}
-
-    void setAnnotation(char &buff) {
-       memcpy(this->buffer, buff, strlen(buff));
-    }
+  void setAnnotation(char *buff) {
+    std::memcpy(this->buffer, buff, strlen(buff));
+  }
 };
 
 int main(int ac, char **av) {
   if (ac <= 1)
     exit(1);
-  
-  N* n1 = new N(5);
-  N* n2 = new N(6);
+
+  N *n1 = new N(5);
+  N *n2 = new N(6);
   n1->setAnnotation(av[1]);
   *n1 + *n2;
 }
-
-// _Znwj --> new
-// _Znaj --> new []
-// https://reverseengineering.stackexchange.com/questions/4402/what-is-operator-newunsigned-int
