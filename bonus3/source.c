77f40f8c7db7d54b329f36045fceb568838c3b0a
tsiguenz <tsiguenz@student.42.fr>
2024-03-21 11:35:59 +0100

    add includes for bonus3 source

--- bonus3/source.c
+++ bonus3/source.c
@@ -1,26 +1,29 @@
-int main (int ac, char *av[]) {
-    FILE *fd = fopen("/home/user/end/.pass", "r");
-    char buffer[132]; 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
 
-    bzero(buffer, 33);
+int main(int ac, char *av[]) {
+  FILE *fd = fopen("/home/user/end/.pass", "r");
+  char buffer[132];
 
-    if(fd == NULL || ac != 2)
-        return 1;
+  bzero(buffer, 33);
 
-    fread(buffer, 1, 66, fd);
-    buffer[89] = '\0';
+  if (fd == NULL || ac != 2)
+    return 1;
 
-    buffer[atoi(av[1])] = '\0';
+  fread(buffer, 1, 66, fd);
+  buffer[89] = '\0';
 
-    fread(&buffer[66], 1, 65, fd);
-    fclose(fd);
+  buffer[atoi(av[1])] = '\0';
 
-    if (!strcmp(buffer, av[1])) {
-        execl("/bin/sh","sh" , NULL);
-    }
-    else {
-        puts(&buffer[66]);
-    }
-    
-    return 0;
+  fread(&buffer[66], 1, 65, fd);
+  fclose(fd);
+  if (!strcmp(buffer, av[1])) {
+    execl("/bin/sh", "sh", NULL);
+  } else {
+    puts(&buffer[66]);
+  }
+  return 0;
 }
